ba60cc228fda41a55083bf223893ce9039859f7d
Jared <jaredlll08@gmail.com>
2021-08-27 23:43:54 +0200

    expose heat condition

---
+++ src/main/java/com/blamejared/createtweaker/natives/ExpandHeatCondition.java
@@ -0,0 +1,11 @@
+package com.blamejared.createtweaker.natives;
+
+import com.blamejared.crafttweaker.api.annotations.ZenRegister;
+import com.blamejared.crafttweaker_annotations.annotations.NativeTypeRegistration;
+import com.simibubi.create.content.contraptions.processing.HeatCondition;
+
+@ZenRegister
+@NativeTypeRegistration(value = HeatCondition.class, zenCodeName = "mods.create.HeatCondition")
+public class ExpandHeatCondition {
+
+}
