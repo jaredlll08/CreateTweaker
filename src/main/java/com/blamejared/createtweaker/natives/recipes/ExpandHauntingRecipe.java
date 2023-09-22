f065197e39fe006a8e0e524d7cadd0a55c57ebb2
oier <oierbravo@gmail.com>
2023-09-22 22:17:54 +0200

    update for create 0.5.1.e

--- src/main/java/com/blamejared/createtweaker/natives/recipes/ExpandHauntingRecipe.java
+++ src/main/java/com/blamejared/createtweaker/natives/recipes/ExpandHauntingRecipe.java
@@ -3,7 +3,7 @@ package com.blamejared.createtweaker.natives.recipes;
 import com.blamejared.crafttweaker.api.annotation.ZenRegister;
 import com.blamejared.crafttweaker_annotations.annotations.Document;
 import com.blamejared.crafttweaker_annotations.annotations.NativeTypeRegistration;
-import com.simibubi.create.content.kinetics.fan.HauntingRecipe;
+import com.simibubi.create.content.kinetics.fan.processing.HauntingRecipe;
 
 @ZenRegister
 @Document("mods/createtweaker/recipe/type/HauntingRecipe")
